54fb768eac038d168dc21d22a435966110a8ae52
Thanh Phong <phonglethanh2@gmail.com>
2024-03-13 15:33:24 +0700

    fix: minor bugs

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import federation from "@originjs/vite-plugin-federation";
 export default defineConfig({
   plugins: [
     react(),
-    federation({
-      name: "app",
-      remotes: {
-        remoteApp: "http://localhost:5001/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
+    // federation({
+    //   name: "app",
+    //   remotes: {
+    //     remoteApp: "http://localhost:5001/assets/remoteEntry.js",
+    //   },
+    //   shared: ["react", "react-dom"],
+    // }),
   ],
   esbuild:{
     drop: ['console', 'debugger']
